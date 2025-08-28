b46e0e3ff4da7b98c727ba91667e46fc26a3934e
aranghatacademy <aranghatacademy@outlook.com>
2025-08-28 14:05:04 +0530

    Add project files.

---
+++ ClassNameAnalyzer/ClassNameAnalyzer/ClassNameAnalyzerAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ClassNameAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ClassNameAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CUT001";
+
+        private static readonly LocalizableString Title = "Invalid Class Name";
+        private static readonly LocalizableString MessageFormat = "Class Name '{0}' should contain only Alphabets ";
+        private static readonly LocalizableString Description = "Class Name must be PascalCase and must contain only Alphabets and no Special chars or numbers";
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category
+            , DiagnosticSeverity.Error
+            , isEnabledByDefault: true
+            , description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public static Regex PascalCaseClassNameRegex = new Regex("^[A-Z][a-zA-Z]+$",RegexOptions.Compiled);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+           
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+       
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var className = context.Symbol.Name;
+            if(!PascalCaseClassNameRegex.IsMatch(className))
+            {
+                
+                var diagnostic = Diagnostic.Create(Rule, context.Symbol.Locations[0], context.Symbol.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ NamesSpaceAnalyzer/NamesSpaceAnalyzer/NamesSpaceAnalyzerAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace NamesSpaceAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NamesSpaceAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ART001";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = "Namespace must be of the format Company.Product.Feature";
+        private static readonly LocalizableString MessageFormat = "Namespace '{0}' does not start with 'Company.Product.Feature'";
+        private static readonly LocalizableString Description = "Namespaces must begin with 'Company.Product.Feature'. e.g Aranghat.MyAwsomeProcut.Access";
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat
+            , Category
+            , DiagnosticSeverity.Error
+            , isEnabledByDefault: true
+            , description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+           
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Namespace);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var nameSpaceName = ((INamespaceSymbol)context.Symbol).ToDisplayString();
+            var isValid =  Regex.IsMatch(nameSpaceName, "^Aranghat\\.[A-Z][a-zA-Z0-9]*\\.[A-Z][a-zA-Z0-9]*$");
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (!isValid)
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, context.Symbol.Locations[0], nameSpaceName);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
