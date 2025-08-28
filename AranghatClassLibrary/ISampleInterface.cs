3dd4b7f9da9eb1aa70531197efac8ec1f17a3600
aranghatacademy <aranghatacademy@outlook.com>
2025-08-28 14:26:45 +0530

    Added a template class library

---
+++ AranghatClassLibrary/ISampleInterface.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Product : Aranghat Class Library
+ * Company : Aranghat Technologies Private Limited
+ * Copyright : All rights reserved
+ * Author : Sreehari Aranghat
+ * 
+ * Change History :
+ * 
+ * [Date]        [Author]        [Description] 
+ * 
+ * */
+
+namespace Aranghat.Product.Feature
+{
+    /// <summary>
+    /// Provide Comment for documentation
+    /// </summary>
+    public interface ISampleInterface
+    {
+        #region Properties
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Events
+        #endregion
+    }
+}
