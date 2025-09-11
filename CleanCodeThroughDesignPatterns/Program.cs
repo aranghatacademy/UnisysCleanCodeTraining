377df68e806434b3d56eacaf76deb2393ef4eb64
aranghatacademy <aranghatacademy@outlook.com>
2025-09-11 10:28:28 +0530

    Added Generic Strategy

--- CleanCodeThroughDesignPatterns/Program.cs
+++ CleanCodeThroughDesignPatterns/Program.cs
@@ -2,7 +2,7 @@
 
 using CleanCodeThroughDesignPatterns.StratagyPAttern;
 
-Console.WriteLine("How do you like to pay? (CreditCard/PayPal/UPI)");
+/*Console.WriteLine("How do you like to pay? (CreditCard/PayPal/UPI)");
 var paymentType = Console.ReadLine();
 
 //FActory Method : Choose and create the instance based on the condition
@@ -11,4 +11,10 @@ IPaymentStratagy paymentStrategy = PaymentStrategyFactory.CreatePaymentStratagy(
 // Dependency Injection : Inject the dependency via constructor
 //Provide a stratagy during runtime which the processor will use
 var processor = new PaymentProcessorStrategy(paymentStrategy);
-processor.ProcessPayment(1000);
+processor.ProcessPayment(1000);*/
+
+var paymentProcess = new StrategyContext<PaymentRequest, PaymentResponse>(new CreditCardPaymentStrategy());
+paymentProcess.ExecuteStrategy(new PaymentRequest { Amount = 100 });
+
+var messageProcess = new StrategyContext<Message, MessageResponse>(new EmailMessageSender());
+messageProcess.ExecuteStrategy(new Message { Content = "Test MEssage", Email = "sree@gmail.com" });
---
+++ CleanCodeThroughDesignPatterns/StratagyPAttern/StrategyPatternWithGenerics.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeThroughDesignPatterns.StratagyPAttern
+{
+    public  interface IStrategy<T,TResult>
+    {
+        TResult Execute(T input);
+    }
+
+    public class StrategyContext<T, TResult>
+    {
+        private readonly IStrategy<T, TResult> _strategy;
+        public StrategyContext(IStrategy<T, TResult> strategy)
+        {
+            _strategy = strategy;
+        }
+        public TResult ExecuteStrategy(T input)
+        {
+            return _strategy.Execute(input);
+        }
+    }
+
+    // Strategy Used for Payment
+    public class  PaymentRequest
+    {
+        public decimal Amount { get; set; }
+    }
+
+    public class PaymentResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class CreditCardPaymentStrategy : IStrategy<PaymentRequest, PaymentResponse>
+    {
+        public PaymentResponse Execute(PaymentRequest input)
+        {
+            // Process Credit Card Payment
+            return new PaymentResponse { IsSuccess = true, Message = $"Processed credit card payment of {input.Amount}" };
+        }
+    }
+
+    public class PayPalPaymentStrategy : IStrategy<PaymentRequest, PaymentResponse>
+    {
+        public PaymentResponse Execute(PaymentRequest input)
+        {
+            // Process PayPal Payment
+            return new PaymentResponse { IsSuccess = true, Message = $"Processed PayPal payment of {input.Amount}" };
+        }
+    }
+
+
+   //Strategy To Sent a Message
+   public class Message
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class MessageResponse
+    {
+        public bool IsSent { get; set; }
+        public string StatusMessage { get; set; }
+    }
+
+    public class SMSMEssageSender : IStrategy<Message, MessageResponse>
+    {
+        public MessageResponse Execute(Message input)
+        {
+            // Send SMS
+            return new MessageResponse { IsSent = true, StatusMessage = $"SMS sent to {input.PhoneNumber}" };
+        }
+    }
+
+    public class EmailMessageSender : IStrategy<Message, MessageResponse>
+    {
+        public MessageResponse Execute(Message input)
+        {
+            // Send Email
+            return new MessageResponse { IsSent = true, StatusMessage = $"Email sent to {input.Email}" };
+        }
+    }
+
+}
